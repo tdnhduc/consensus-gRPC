aa0188d0eece2e99924ec066341066514eec5bcf
DucNguyen <ducnguyenpy1221@gmail.com>
2020-12-16 23:14:02 +0700

    fix logic consensus

--- consensus/src/main/java/com/hcmus/distributed/system/consensus/server/impl/BroadcastServiceImpl.java
+++ consensus/src/main/java/com/hcmus/distributed/system/consensus/server/impl/BroadcastServiceImpl.java
@@ -9,14 +9,11 @@ import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 
-import java.io.IOException;
-import java.sql.Time;
 import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class BroadcastServiceImpl extends ConsensusServiceGrpc.ConsensusServiceImplBase {
@@ -41,6 +38,13 @@ public class BroadcastServiceImpl extends ConsensusServiceGrpc.ConsensusServiceI
                                                                                 request.getBaseRequest().getPid(),
                                                                                 request.getPidNeedConfirm(),
                                                                                 request.getIsLegal());
+        calculateQuantumConfirm(request);
+        BaseResponse baseResponse = BaseResponse.newBuilder().setTimeReceive(System.currentTimeMillis())
+                                                            .setPid(this.processInfo.getPid())
+                                                            .setAck(true).build();
+        BroadcastResponse response = BroadcastResponse.newBuilder().setBaseResponse(baseResponse).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 
     @Override
@@ -65,11 +69,35 @@ public class BroadcastServiceImpl extends ConsensusServiceGrpc.ConsensusServiceI
     }
 
     private boolean validateScheduleRequest(BaseRequest request) {
-        return true;
+        Date date = TimeParser.parseCurrentTime(request.getTimeSend());
+        long seconds = (date.getMinutes() * 60 + date.getSeconds()) * 1000;
+        while(seconds >= this.processInfo.getFixRated()) {
+            seconds -= this.processInfo.getFixRated();
+        }
+        String pidExpect = "";
+        seconds += this.processInfo.getMapTimeDelay().get(request.getPid());
+        for(String pid : this.processInfo.getMapTimeDelay().keySet()) {
+            if (seconds >= this.processInfo.getMapTimeDelay().get(pid)) {
+                pidExpect = pid;
+            } else {
+                break;
+            }
+        }
+        LOGGER.info("Time = {}", seconds);
+        if (pidExpect.isEmpty()) {
+            LOGGER.error("Cannot calculate pid expect from request!!!");
+            return false;
+        }
+        if (pidExpect.equals(request.getPid())) {
+            LOGGER.info("[VALID] schedule request pid = {}, pidExpect = {}", request.getPid(), pidExpect);
+            return true;
+        } else {
+            LOGGER.info("[INVALID] schedule request pid = {}, pidExpect = {}", request.getPid(), pidExpect);
+            return false;
+        }
     }
 
     private void pushIntoConfirmMap(BaseRequest request) {
-        //TODO validate schedule request
         AtomicInteger numbConfirm = this.confirmMap.get(request.getPid());
         if (numbConfirm == null) {
             numbConfirm = new AtomicInteger(0);
@@ -85,12 +113,34 @@ public class BroadcastServiceImpl extends ConsensusServiceGrpc.ConsensusServiceI
     }
 
     private void doBroadCast(BroadcastRequest request) {
+        int index = 0;
         for (ConsensusServiceGrpc.ConsensusServiceBlockingStub stub : this.stubMap.values()) {
             try {
-                stub.broadcast(request);
+                stub.withDeadlineAfter(3L, TimeUnit.SECONDS).broadcast(request);
+                index ++;
             } catch (StatusRuntimeException e) {
                 LOGGER.debug("DEADLINE EXCEED");
             }
+        }
+        LOGGER.info("[BROADCAST] total broadcast to {} nodes", index);
+    }
+
+    private synchronized void calculateQuantumConfirm (BroadcastRequest request) {
+        if (request.getIsLegal()) {
+            AtomicInteger confirm = this.confirmMap.get(request.getPidNeedConfirm());
+            if (confirm == null) {
+                confirm = new AtomicInteger(1);
+            } else {
+                if (confirm.incrementAndGet() >= this.processInfo.getNumberConfirm()) {
+                    LOGGER.info("[CONFIRM] enough confirm, write to stable storage by pid {}", request.getPidNeedConfirm());
+                    this.confirmMap.remove(request.getPidNeedConfirm());
+                    return;
+                }
+            }
+            this.confirmMap.put(request.getPidNeedConfirm(), confirm);
+            LOGGER.debug("increase confirm of node {} by one", request.getPidNeedConfirm());
+        } else {
+            LOGGER.debug("Not enough confirm, wait in {} milliseconds to maybe enough confirm", this.processInfo.getFixRated());
         }
     }
 }
